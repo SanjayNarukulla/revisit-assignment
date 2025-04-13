80e032cb79c15e3471a68cb259c4ccf4ea7413cd
SanjayNarukulla <narukullasanjay@gmail.com>
2025-04-13 21:57:44 +0530

    Fixed build config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensures that the build outputs to the 'dist' folder
+    outDir: "dist", // Ensure that the output directory is `dist`
   },
 });
